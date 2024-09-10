7c059c08b33ab57e982463d0a29589c31ccf67f9
Pop Suwichapol Jinnawong <pop_sg88@hotmail.com>
2024-09-10 14:16:48 +0700

    feat: Prepare route versioning

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,20 +10,29 @@ use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
-        health: '/up',
-        then: function () {
+        using: function () {
+            Route::middleware('api')
+                ->prefix('api/v1')
+                ->group(base_path('routes/auth.php'));
+
+            Route::middleware('api')
+                ->prefix('api/v1')
+                ->group(base_path('routes/api.php'));
+
+            Route::middleware('web')
+                ->group(base_path('routes/web.php'));
+
             if (! app()->environment('production')) {
                 Route::middleware('web')
                     ->group(base_path('routes/dev_web.php'));
 
                 Route::middleware('api')
+                    ->prefix('api/v1')
                     ->group(base_path('routes/dev_api.php'));
             }
-
-        }
+        },
+        commands: __DIR__.'/../routes/console.php',
+        health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->trustProxies(at: '*');
