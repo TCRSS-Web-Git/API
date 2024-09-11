89b5960e1f58d2e5562507324989e8dd813b1b30
Pop Suwichapol Jinnawong <pop_sg88@hotmail.com>
2024-09-11 17:13:25 +0700

    feat: Add response trait

--- app/Http/Controllers/User/InviteController.php
+++ app/Http/Controllers/User/InviteController.php
@@ -2,19 +2,27 @@
 
 namespace App\Http\Controllers\User;
 
+use App\Actions\User\SaveUserInvitation;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\InviteUserRequest;
 use App\Models\User;
+use App\Traits\ResponseTrait;
 use Illuminate\Support\Facades\Gate;
+use Symfony\Component\HttpFoundation\Response;
 
 class InviteController extends Controller
 {
-    public function invite(InviteUserRequest $request)
+    use ResponseTrait;
+
+    public function invite(InviteUserRequest $request, SaveUserInvitation $saveUserInvitation)
     {
         Gate::authorize('create', User::class);
 
         $data = $request->validated();
 
+        $saveUserInvitation->execute($data);
+
+        return $this->successResponse([], Response::HTTP_NO_CONTENT);
     }
 
     public function resend() {}
