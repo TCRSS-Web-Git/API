3a61d63f5e10a2b918aad8861e156ad180eeb4b0
Pop Suwichapol <pop_sg88@hotmail.com>
2024-10-03 19:52:26 +0700

    chore: Fix pint

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\User;
 
 use App\Actions\User\SaveUserInvitation;
-use App\Enums\Permission;
 use App\Filters\UserFilter;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\CreateUserRequest;
