9c42353c035dac8568554ebe54f8f1337f50aa19
Noppadon Tongtrakoon <notenoppadon5019@gmail.com>
2024-10-08 12:05:42 +0700

    chore: Add testcase for career index api

--- tests/Feature/JobPostTest.php
+++ tests/Feature/JobPostTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use App\Enums\JobPostStatus;
 use App\Models\Category;
 use App\Models\JobPost;
+use Carbon\Carbon;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -31,6 +32,160 @@ class JobPostTest extends TestCase
     }
 
     /**
+     * Test the admin can get filtered jobs by search.
+     */
+    public function test_the_admin_can_get_jobs_filtered_by_search(): void
+    {
+        // set up
+        $this->signInAdmin();
+        [$jobPostA, $jobPostB, $jobPostC] = JobPost::factory()->count(3)->create();
+        $jobPostA->setTranslation('title', 'test A', 'en');
+        $jobPostA->save();
+        $jobPostB->setTranslation('title', 'B test', 'en');
+        $jobPostB->save();
+        $jobPostC->setTranslation('title', 'job post C', 'en');
+        $jobPostC->save();
+
+        // act
+        $response = $this->getJson(route('careers.index', ['search' => 'test']));
+
+        // assert
+        $response->assertOk();
+        $response->assertJsonFragment(['id' => $jobPostA->hashid]);
+        $response->assertJsonFragment(['id' => $jobPostB->hashid]);
+        $response->assertJsonMissing(['id' => $jobPostC->hashid]);
+    }
+
+    /**
+     * Test the admin can get filtered jobs by departments.
+     */
+    public function test_the_admin_can_get_jobs_filtered_by_department(): void
+    {
+        // set up
+        $this->signInAdmin();
+        $departmentA = Category::factory()->jobPost()->create();
+        $departmentB = Category::factory()->jobPost()->create();
+        $departmentC = Category::factory()->jobPost()->create();
+
+        $jobPost1 = JobPost::factory()->create(['department_id' => $departmentA->id]);
+        $jobPost2 = JobPost::factory()->create(['department_id' => $departmentB->id]);
+        $jobPost3 = JobPost::factory()->create(['department_id' => $departmentC->id]);
+        $jobPost4 = JobPost::factory()->create(['department_id' => $departmentA->id]);
+
+        // act
+        $response = $this->getJson(route('careers.index', ['department_id' => "$departmentA->hashid,$departmentB->hashid"]));
+
+        // assert
+        $response->assertOk();
+        $response->assertJsonFragment(['id' => $jobPost1->hashid]);
+        $response->assertJsonFragment(['id' => $jobPost2->hashid]);
+        $response->assertJsonFragment(['id' => $jobPost4->hashid]);
+        $response->assertJsonMissing(['id' => $jobPost3->hashid]);
+
+        // act
+        $response = $this->getJson(route('careers.index', ['department_id' => "$departmentC->hashid"]));
+
+        // assert
+        $response->assertOk();
+        $response->assertJsonFragment(['id' => $jobPost3->hashid]);
+        $response->assertJsonMissing(['id' => $jobPost1->hashid]);
+        $response->assertJsonMissing(['id' => $jobPost2->hashid]);
+        $response->assertJsonMissing(['id' => $jobPost4->hashid]);
+    }
+
+    /**
+     * Test the admin can get filtered jobs by location.
+     */
+    public function test_the_admin_can_get_jobs_filtered_by_location(): void
+    {
+        // set up
+        $this->signInAdmin();
+        $locationA = Category::factory()->location()->create();
+        $locationB = Category::factory()->location()->create();
+        $locationC = Category::factory()->location()->create();
+
+        $jobPost1 = JobPost::factory()->create(['location_id' => $locationA->id]);
+        $jobPost2 = JobPost::factory()->create(['location_id' => $locationB->id]);
+        $jobPost3 = JobPost::factory()->create(['location_id' => $locationC->id]);
+        $jobPost4 = JobPost::factory()->create(['location_id' => $locationA->id]);
+
+        // act
+        $response = $this->getJson(route('careers.index', ['location_id' => "$locationA->hashid,$locationB->hashid"]));
+
+        // assert
+        $response->assertOk();
+        $response->assertJsonFragment(['id' => $jobPost1->hashid]);
+        $response->assertJsonFragment(['id' => $jobPost2->hashid]);
+        $response->assertJsonFragment(['id' => $jobPost4->hashid]);
+        $response->assertJsonMissing(['id' => $jobPost3->hashid]);
+
+        // act
+        $response = $this->getJson(route('careers.index', ['location_id' => "$locationC->hashid"]));
+
+        // assert
+        $response->assertOk();
+        $response->assertJsonFragment(['id' => $jobPost3->hashid]);
+        $response->assertJsonMissing(['id' => $jobPost1->hashid]);
+        $response->assertJsonMissing(['id' => $jobPost2->hashid]);
+        $response->assertJsonMissing(['id' => $jobPost4->hashid]);
+    }
+
+    /**
+     * Test the admin can get filtered jobs by status.
+     */
+    public function test_the_admin_can_get_jobs_filtered_by_status(): void
+    {
+        // set up
+        Carbon::setTestNow(Carbon::now());
+        $this->signInAdmin();
+        $jobPost1 = JobPost::factory()->create(['published_at' => Carbon::now()]);
+        $jobPost2 = JobPost::factory()->create(['published_at' => Carbon::now()->subSecond()]);
+        $jobPost3 = JobPost::factory()->create(['published_at' => Carbon::now()->subDay()]);
+        $jobPost4 = JobPost::factory()->create(['published_at' => Carbon::now()->addSecond()]);
+        $jobPost5 = JobPost::factory()->create(['published_at' => Carbon::now()->addDay()]);
+
+        // act
+        $response = $this->getJson(route('careers.index', ['status' => 'published']));
+
+        // assert
+        $response->assertOk();
+        $response->assertJsonFragment(['id' => $jobPost1->hashid]);
+        $response->assertJsonFragment(['id' => $jobPost2->hashid]);
+        $response->assertJsonFragment(['id' => $jobPost3->hashid]);
+        $response->assertJsonMissing(['id' => $jobPost4->hashid]);
+        $response->assertJsonMissing(['id' => $jobPost5->hashid]);
+
+        // act
+        $response = $this->getJson(route('careers.index', ['status' => 'draft']));
+
+        // assert
+        $response->assertOk();
+        $response->assertJsonFragment(['id' => $jobPost4->hashid]);
+        $response->assertJsonFragment(['id' => $jobPost5->hashid]);
+        $response->assertJsonMissing(['id' => $jobPost1->hashid]);
+        $response->assertJsonMissing(['id' => $jobPost2->hashid]);
+        $response->assertJsonMissing(['id' => $jobPost3->hashid]);
+    }
+
+    /**
+     * Test the admin can get a job by ID.
+     */
+    public function test_the_admin_can_get_a_job_by_id(): void
+    {
+        // set up
+        $this->signInAdmin();
+        $jobPost = JobPost::factory()->create();
+
+        // act
+        $response = $this->getJson(route('careers.show', $jobPost));
+
+        // assert
+        $response->assertOk();
+        $response->assertJsonFragment(['id' => $jobPost->hashid]);
+        $response->assertJsonFragment(['title' => $jobPost->title]);
+    }
+
+    /**
      * Test the admin can get a job by ID.
      */
     public function test_the_admin_can_get_a_job_by_id_localized(): void
@@ -54,6 +209,28 @@ class JobPostTest extends TestCase
         $response->assertOk();
         $response->assertJsonFragment(['id' => $jobPost->hashid]);
         $response->assertJsonFragment(['title' => $jobPost->getTranslation('title', 'en')]);
+    }
+
+    /**
+     * Test the admin can get a job by ID with all translations.
+     */
+    public function test_the_admin_can_get_a_job_by_id_with_all_translations(): void
+    {
+        // set up
+        $this->signInAdmin();
+        $jobPost = JobPost::factory()->create();
+
+        // act
+        $response = $this->getJson(route('careers.show', ['career' => $jobPost, 'include' => 'translations']));
+
+        // assert
+        $response->assertOk();
+        $response->assertJsonFragment(['id' => $jobPost->hashid]);
+        $response->assertJsonFragment(['title' => $jobPost->title]);
+        $response->assertJsonFragment(['title' => $jobPost->getTranslation('title', 'th')]);
+        $response->assertJsonFragment(['title' => $jobPost->getTranslation('title', 'en')]);
+        $response->assertJsonStructure(['data' => ['translations' => ['th' => ['title']]]]);
+        $response->assertJsonStructure(['data' => ['translations' => ['en' => ['title']]]]);
     }
 
     public function test_the_admin_can_create_a_published_job_post(): void
