157648e921ec17939d8b32fcd054a7714aae6b4d
SivakornSuttisom <sutitsom@gmail.com>
2024-10-17 16:32:12 +0700

    chore: Fix code

--- app/Http/Resources/AnnualReportResource.php
+++ app/Http/Resources/AnnualReportResource.php
@@ -4,7 +4,6 @@ namespace App\Http\Resources;
 
 use App\Enums\AnnualReportStatus;
 use App\Models\AnnualReport;
-use App\Models\AnnualReport;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
