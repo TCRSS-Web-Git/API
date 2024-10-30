c36d3c6abc35e70a538bf281a7556983af5cfa0a
Noppadon Tongtrakoon <notenoppadon5019@gmail.com>
2024-10-30 15:26:55 +0700

    chore: Fix code style

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 use App\Enums\CategoryType;
 use App\Models\Category;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class CategorySeeder extends Seeder
