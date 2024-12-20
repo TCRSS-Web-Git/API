b10b3a7ad4aa6b52cf1eef9a1c9768684278437f
Noppadon Tongtrakoon <notenoppadon5019@gmail.com>
2024-12-20 18:09:29 +0700

    feat: Fix server config

--- deployment/startup.sh
+++ deployment/startup.sh
@@ -3,4 +3,8 @@ apt-get update && apt-get install -y libfreetype6-dev \
                 libpng-dev \
                 libwebp-dev \
         && docker-php-ext-configure gd --with-freetype --with-webp  --with-jpeg
-docker-php-ext-install gd
+docker-php-ext-install gd &&
+mkdir -p /home/site/ini &&
+cp /home/site/wwwroot/deployment/startup.sh /home/site/ini/startup.sh &&
+cp /home/site/wwwroot/deployment/extensions.ini /home/site/ini/extensions.ini &&
+cp /home/site/wwwroot/deployment/default /etc/nginx/sites-available/default && service nginx reload
