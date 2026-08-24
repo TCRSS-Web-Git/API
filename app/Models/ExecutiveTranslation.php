e4ddc3a7149ece2866f08f1a46abe3506973175d
Suradech-Wec <suradech_wec@tcrss.com>
2026-08-24 09:33:07 +0700

    Add files via upload

---
+++ app/Models/ExecutiveTranslation.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use LaravelLang\Models\Casts\TrimCast;
+use LaravelLang\Models\Eloquent\Translation;
+use OwenIt\Auditing\Contracts\Auditable;
+
+class ExecutiveTranslation extends Translation implements Auditable
+{
+    use \OwenIt\Auditing\Auditable;
+
+    protected $touches = ['parent'];
+
+    protected $fillable = [
+        'locale',
+        'name',
+        'position',
+    ];
+
+    protected $casts = [
+        'name' => TrimCast::class,
+        'position' => TrimCast::class,
+    ];
+
+    public function search($query)
+    {
+        return self::whereRaw('MATCH(name, position) AGAINST(? IN BOOLEAN MODE)', [$query]);
+    }
+
+    public function parent(): BelongsTo
+    {
+        return $this->belongsTo(Executive::class, 'item_id');
+    }
+}
